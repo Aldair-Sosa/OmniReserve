2b621f679957d012e959c503b72066f8084b2900
Aldair-Sosa <eduardo.aldair.sosa@gmail.com>
2026-09-18 14:26:58 -0600

    Actividad 4: El proyecto compilo de manera correcta, se agregaron los servicios de el paquete nuget y se agrego en el Command y el handles para la primer comando crear habitacion

--- OmniReserve.Application/Rooms/Commands/CreateRoomCommandHandler.cs
+++ OmniReserve.Application/Rooms/Commands/CreateRoomCommandHandler.cs
@@ -5,6 +5,8 @@ namespace OmniReserve.Application.Rooms.Commands.CreateRoom;
 
 public class CreateRoomCommandHandler : IRequestHandler<CreateRoomCommand, Guid>
 {
+
+    //El programa compilo de manera correcta.
     public Task<Guid> Handle(CreateRoomCommand request, CancellationToken cancellationToken)
     {
         var room = new Room(request.RoomNumber, request.Type, request.PricePerNight);
