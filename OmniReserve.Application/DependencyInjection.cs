4f281c25565a1cfd5c4d70243b985bda0c7b0327
Aldair-Sosa <eduardo.aldair.sosa@gmail.com>
2026-09-18 14:17:09 -0600

    Actividad 4: Se instalo el paquete nuget mediatR, se agrego el servicio en dependencyIngjection, se creo el Command y el headler para el comando crearHabitacion

--- OmniReserve.Application/DependencyInjection.cs
+++ OmniReserve.Application/DependencyInjection.cs
@@ -1,10 +1,12 @@
 using Microsoft.Extensions.DependencyInjection;
+using  System.Reflection;
 
 namespace OmniReserve.Application;
 public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
         return services;
     }
