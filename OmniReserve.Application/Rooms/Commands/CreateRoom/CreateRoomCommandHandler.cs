cd468f628c2235904e77581d271fd35bca13f728
Aldair-Sosa <eduardo.aldair.sosa@gmail.com>
2026-09-21 17:08:49 -0600

    Actividad 4: Se agrego el paquete en la carpeta .Application y se registro el servicio en independncyIjection y se creo el comando para la creacion de CreateRoom

--- OmniReserve.Application/Rooms/Commands/CreateRoom/CreateRoomCommandHandler.cs
+++ OmniReserve.Application/Rooms/Commands/CreateRoom/CreateRoomCommandHandler.cs
@@ -6,7 +6,6 @@ namespace OmniReserve.Application.Rooms.Commands.CreateRoom;
 public class CreateRoomCommandHandler : IRequestHandler<CreateRoomCommand, Guid>
 {
 
-    //El programa compilo de manera correcta.
     public Task<Guid> Handle(CreateRoomCommand request, CancellationToken cancellationToken)
     {
         var room = new Room(request.RoomNumber, request.Type, request.PricePerNight);
