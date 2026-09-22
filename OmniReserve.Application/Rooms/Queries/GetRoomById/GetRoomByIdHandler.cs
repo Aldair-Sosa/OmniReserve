da41e9c04306be008db4628ad6db349c17ab40cd
Aldair-Sosa <eduardo.aldair.sosa@gmail.com>
2026-09-21 18:52:01 -0600

    Terea-4 : Se agrego la carpeta Rooms /Queries y se se agregaron los metodos GetCommandById con sus respectivos handler

---
+++ OmniReserve.Application/Rooms/Queries/GetRoomById/GetRoomByIdHandler.cs
@@ -0,0 +1,8 @@
+using MediatR;
+using OmniReserve.Application.Rooms.Queries.GetRoomById;
+namespace OmniReserve.Application.Rooms.Queries.GetRoomById;
+
+public static class GetRoomByIdQueryHandler : IRequestHandler<GetRoomByIdQuery, RoomResponseDto>
+{
+    
+}
